828467310aab4663a73e74fbb3d52045c7783ab1
jsrice7391 <jsrice7391@gmail.com>
2019-01-08 08:08:18 -0500

    First commit. There is some of the jQuery stuff done. I need to add PLEASE FORK to the markdown

---
+++ assets/main.js
@@ -0,0 +1,14 @@
+console.log("Welcome to the JQuery Test");
+
+var boxCount = 3;
+
+//Change the header in the index.html page to your name.
+
+//Create 3 boxes with the class box and an ID of a number EX: id="one" or id="boxOne"
+// You can do this dynamially or directly on the html page. 
+
+//When I click on box one turn it blue 
+
+//When I click on box 2, double the height of box3
+
+// When I click the reset button, return all boxes to their original states.
